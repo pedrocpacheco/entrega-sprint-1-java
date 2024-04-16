d276730999c17ff1bf150fb402bcfb71c13ff566
pedrocpacheco <cpedropacheco@hotmail.com>
2024-04-15 22:30:15 -0300

    Initial Commit

---
+++ src/main/java/br/com/genesisacademy/GenesisacademyApplication.java
@@ -0,0 +1,13 @@
+package br.com.genesisacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesisacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/genesisacademy/GenesisacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.genesisacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
